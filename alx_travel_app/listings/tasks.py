a3b3ff73f435ed88bcda1872b9fc1b51ac3252f2
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-08-25 12:02:01 +0300

    Fixes and Updates

--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -4,10 +4,19 @@ from django.conf import settings
 
 @shared_task
 def send_payment_confirmation_email(user_email, booking_id, amount):
+    """
+    Send payment confirmation email asynchronously.
+    """
     subject = "Payment Confirmation"
     message = f"""
+    Dear Customer,
+
     Your payment for Booking #{booking_id} was successful.
-    Amount paid: {amount} KES
-    Thank you for using our service.
+    Amount Paid: {amount} KES
+
+    Thank you for using our service!
     """
+
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email])
+
+    return f"Payment confirmation email sent to {user_email}"
